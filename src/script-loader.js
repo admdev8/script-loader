2159b7a50bda5add10abee4cba51c71c25889dce
Maksim Chekryshov <m.chekryshov@rambler-co.ru>
2015-10-01 11:45:02 +0300

    Now:  When page already contains script with equivalent src it will be replaced without duplication

--- src/script-loader.js
+++ src/script-loader.js
@@ -3,7 +3,7 @@
  * Created by m.chekryshov on 25.08.15.
  */
 
-// Use this util to load some third party script
+  // Use this util to load third party script asynchronously
 (function(window) {
   if (!window || window.ScriptLoader) {
     return;
@@ -11,79 +11,133 @@
 
   var scriptsQueue = {};
 
-  function appendToHead(src, callback) {
+  /**
+   * Append script to the head section
+   * When page already contains script with equivalent src it will be replaced without duplication
+   *
+   * @param {string} src - script src
+   * @param {string} srcName - mnemonic name of script
+   */
+  function appendToHead(src, srcName) {
     var script = window.document.createElement('script');
     script.type = 'text/javascript';
+    script.src = src;
 
-    if (script.readyState) {  // IE
+    if (script.readyState) { // IE
       script.onreadystatechange = function() {
         if (script.readyState === 'loaded' ||
-          script.readyState === 'complete') {
+            script.readyState === 'complete') {
           script.onreadystatechange = null;
-          callback();
+          setTimeout(function() {
+            triggerCallbacks(srcName, 'success');
+          }, 0);
         }
       };
-    } else {  // others
+    } else { // others
       script.onload = function() {
-        callback();
+        triggerCallbacks(srcName, 'success');
+      };
+      script.onerror = function() {
+        triggerCallbacks(srcName, 'error');
       };
     }
-
-    script.src = src;
-    window.document.getElementsByTagName('head')[0].appendChild(script);
+    addOrReloadScript(script);
   }
 
-  function triggerCallbacks(srcName) {
-    if (!scriptsQueue[srcName]) {
+  function addOrReloadScript(script) {
+    var scripts = document.getElementsByTagName('script');
+    var scriptToReplace;
+
+    // try remove script if it already loaded
+    for (var i = 0; i < scripts.length; i++) {
+      var currentScriptSrc = scripts[i].src;
+      // check script src ending
+      if (currentScriptSrc.indexOf(script.src, currentScriptSrc.length - script.src.length) !== -1) {
+        scriptToReplace = scripts[i];
+        break;
+      }
+    }
+
+    if (scriptToReplace) {
+      var scriptParentNode = scriptToReplace.parentNode;
+
+      scriptParentNode.insertBefore(script, scriptToReplace);
+      scriptParentNode.removeChild(scriptToReplace);
+    } else {
+      var firstScript = document.getElementsByTagName('script')[0];
+      firstScript.parentNode.insertBefore(script, firstScript);
+    }
+  }
+
+  function triggerCallbacks(srcName, callbackName) {
+    if (isArray(scriptsQueue[srcName])) {
+      for (var i = 0; i < scriptsQueue[srcName].length; i++) {
+        var callback = scriptsQueue[srcName][i][callbackName];
+        callback && callback();
+      }
+    }
+
+    scriptsQueue[srcName] = callbackName;
+  }
+
+  function isArray(arg) {
+    return Object.prototype.toString.call(arg) === '[object Array]';
+  }
+
+  function addCallbacks(srcName, successCallback, errorCallback) {
+    if (!successCallback && !errorCallback) {
       return;
     }
 
-    for (var i = 0; i < scriptsQueue[srcName].length; i++) {
-      scriptsQueue[srcName][i]();
+    if (!isArray(scriptsQueue[srcName])) {
+      scriptsQueue[srcName] = [];
     }
 
-    scriptsQueue[srcName] = null;
+    scriptsQueue[srcName].push({
+      success: successCallback,
+      error: errorCallback
+    });
   }
 
   window.ScriptLoader = {
 
     /**
-     * Load some third party script
+     * Add or reload third party script asynchronously
+     * When page already contains script with equivalent src it will be replaced without duplication
      *
      * @param {string} src - script src
-     * @param {string} srcName - some mnemonic name of script\
-     * @param {Function} [callback]
+     * @param {string} srcName - mnemonic name of script
+     * @param {Function} [successCallback]
+     * @param {Function} [errorCallback]
      */
-    load: function(src, srcName, callback) {
+    load: function(src, srcName, successCallback, errorCallback) {
       if (!src || !srcName) {
         return;
       }
 
-      scriptsQueue[srcName] = callback ? [callback] : [];
-
-      appendToHead(src, function() {
-        triggerCallbacks(srcName);
-      });
+      addCallbacks(srcName, successCallback, errorCallback);
+      appendToHead(src, srcName);
     },
 
     /**
-     *  Wait til some script loaded and call callback
+     *  Wait loading the specified script and call callback function on ready
      *
-     * @param {string} srcName - some mnemonic name of script
-     * @param {Function} callback
+     * @param {string} srcName - mnemonic name of the script
+     * @param {Function} successCallback
+     * @param {Function} [errorCallback]
      */
-    wait: function(srcName, callback) {
-      if (!srcName || !callback) {
+    wait: function(srcName, successCallback, errorCallback) {
+      if (!srcName || !successCallback) {
         return;
       }
 
-      if (!scriptsQueue[srcName]) {
-        callback();
-
-        return;
+      if (scriptsQueue[srcName] === 'success') { // script was successfully loaded
+        successCallback();
+      } else if (scriptsQueue[srcName] === 'error') { // an error occurred
+        errorCallback && errorCallback();
+      } else { // script loading in progress or was newer called
+        addCallbacks(srcName, successCallback, errorCallback);
       }
-
-      scriptsQueue[srcName].push(callback);
     }
   };
 })(window);
